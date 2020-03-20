a0203acd894d8899e4a3a7504cb161e7c00cc4c5
Smajda Mate <mate.smajda@gmail.com>
2020-03-20 12:33:50 +0100

    changed date to instant in message model

---
+++ message-sender/src/main/java/com/hw/sender/message/CustomInstantDeserializer.java
@@ -0,0 +1,27 @@
+package com.hw.sender.message;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+public class CustomInstantDeserializer extends JsonDeserializer<Instant> {
+
+    private final DateTimeFormatter dateFormatter;
+
+    public CustomInstantDeserializer(@Value("${dateformat}") String dateFormat) {
+        dateFormatter = ofPattern(dateFormat);
+    }
+
+    @Override
+    public Instant deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        return ZonedDateTime.parse(p.getValueAsString(), dateFormatter).toInstant();
+    }
+}
--- message-sender/src/main/java/com/hw/sender/message/MessageRequest.java
+++ message-sender/src/main/java/com/hw/sender/message/MessageRequest.java
@@ -1,12 +1,20 @@
 package com.hw.sender.message;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.Instant;
 
 @Data
 class MessageRequest {
 
     private String content;
-    private Date timestamp;
+
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = CustomInstantDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+    private Instant timestamp;
 }
