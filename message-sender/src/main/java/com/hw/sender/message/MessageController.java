92382f41a7f47975c6eef32f191dd94ac1751fc1
Smajda Mate <mate.smajda@gmail.com>
2020-03-21 21:19:16 +0100

    permit cross origin requests when sending new messages

--- message-sender/src/main/java/com/hw/sender/message/MessageController.java
+++ message-sender/src/main/java/com/hw/sender/message/MessageController.java
@@ -2,6 +2,7 @@ package com.hw.sender.message;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class MessageController {
 
     private MessageService messageService;
 
+    @CrossOrigin("*")
     @PostMapping(value = "/message", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addMessage(@RequestBody Message message) {
         messageService.sendMessage(message);
