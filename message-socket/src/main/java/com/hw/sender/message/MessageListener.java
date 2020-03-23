84638cb771145d8b7b0a39ba5080e86de3ee9db7
Smajda Mate <mate.smajda@gmail.com>
2020-03-23 10:05:37 +0100

    fixed configuration

--- message-socket/src/main/java/com/hw/sender/message/MessageListener.java
+++ message-socket/src/main/java/com/hw/sender/message/MessageListener.java
@@ -1,29 +1,31 @@
 package com.hw.sender.message;
 
 import com.google.gson.Gson;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Component
 @Slf4j
-@AllArgsConstructor
 public class MessageListener {
 
     private Gson messageDeserializer;
     private SimpMessagingTemplate template;
+    private String wsDestination;
+
+    public MessageListener(Gson messageDeserializer, SimpMessagingTemplate template, @Value("${websocket.destination.messages}") String wsDestination) {
+        this.messageDeserializer = messageDeserializer;
+        this.template = template;
+        this.wsDestination = wsDestination;
+    }
 
     @KafkaListener(topics = "${message.topic}")
     public void addMessage(@RequestBody String messageJson) {
         log.info("Received message: {}", messageJson);
         Message message = messageDeserializer.fromJson(messageJson, Message.class);
-        template.convertAndSend("/topic/messages", message);
+        template.convertAndSend(wsDestination, message);
     }
 }
